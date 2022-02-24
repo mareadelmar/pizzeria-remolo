f465568da1739b4f3944a1ca1420dfcf35240e20
mareadelmar <mar.ortega.v@gmail.com>
2022-02-24 13:05:59 -0300

    Create card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,31 @@
+import { Card, CardMedia, CardContent, Typography, CardActions, IconButton } from "@mui/material";
+import AddBoxIcon from '@mui/icons-material/AddBox';
+
+const CardItem = (item) => {
+    return (
+    <Card sx={{ width: 180 }} key={item.id} className="card-container">
+        <CardMedia 
+            component="img"
+            height="180"
+            image={item.img}
+            alt={item.name}
+        />
+        <CardContent>
+            <Typography
+                component="h2"
+                variant="h6"
+            >{item.name}</Typography>
+            <Typography
+                component="p"
+            >{item.price}</Typography>
+        </CardContent>
+        <CardActions className="flex card-icon">
+            <IconButton>
+                <AddBoxIcon fontSize="large" />
+            </IconButton>
+        </CardActions>
+    </Card>
+    )
+}
+
+export default CardItem;
--- src/components/ListOfCards/index.tsx
+++ src/components/ListOfCards/index.tsx
@@ -1,32 +1,16 @@
-import { Container, Card, CardMedia, CardContent, Typography } from "@mui/material";
+import { Container } from "@mui/material";
+import CardItem from "../Card";
 
-const ListOfCards = ({products}) => {
-    return (
-    <Container>
-            <div className="list-container">
-                {
-                    products.map((item) => (
-                    <Card sx={{ width: 200 }} key={item.id}>
-                        <CardMedia 
-                        component="img"
-                        height="180"
-                        image={item.img}
-                        alt={item.name}
-                        />
-                        <CardContent>
-                        <Typography
-                            component="h2"
-                            variant="h5"
-                        >{item.name}</Typography>
-                        <Typography
-                            component="p"
-                        >{item.price}</Typography>
-                        </CardContent>
-                    </Card>
-                    ))
-                }
-            </div>
-        </Container>
-)}
+const ListOfCards = ({ products }) => {
+	return (
+		<Container>
+			<div className='list-container'>
+				{products.map(item => (
+					<CardItem {...item} />
+				))}
+			</div>
+		</Container>
+	);
+};
 
 export default ListOfCards;
