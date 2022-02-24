ed45a095a783fff543a237e4c939101401774d61
mareadelmar <mar.ortega.v@gmail.com>
2022-02-24 10:06:18 -0300

    Set up

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+
+const Home = () => {
+    return (
+        <>
+          <section>
+              list of categories
+          </section>
+          <section>
+            list of products
+          </section>
+        </>
+    )
+}
+
+export default Home;
