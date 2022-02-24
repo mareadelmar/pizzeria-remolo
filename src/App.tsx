2073a4a159387346ce343d8fc11ecc5e32bbc98e
mareadelmar <mar.ortega.v@gmail.com>
2022-02-24 18:31:09 -0300

    Create router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import './App.css';
-import Home from './pages/Home';
+import "./App.css";
+import Home from "./pages/Home";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 function App() {
-  return (
-    <Home />
-  )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/products/:categoryId' element={<Home />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
