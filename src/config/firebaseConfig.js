9cd384386678ff252f4a927ca26a4b37c8776eb9
mareadelmar <mar.ortega.v@gmail.com>
2022-02-24 19:59:22 -0300

    Add firebase and firebase config

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyBDDWSkzyveKgXKite3wv_vxu0SPzzP-ic",
+	authDomain: "remolo-pizzeria.firebaseapp.com",
+	projectId: "remolo-pizzeria",
+	storageBucket: "remolo-pizzeria.appspot.com",
+	messagingSenderId: "858988689822",
+	appId: "1:858988689822:web:fba9c34137d4227ed47e8f",
+};
+
+const firebaseInit = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseInit.firestore();
+
+export { db };
